36357b5610e5abe1a1256c43b7dbe4ecb03e9116
Aleksey Grechihin <9108921170@dz-led.ru>
2020-08-06 20:44:18 +0300

    Присвоил переменные

---
+++ script/script.js
@@ -0,0 +1,8 @@
+var money = 5;
+const income = 'Прибыль';
+let addExpenses = null;
+const deposit = false;
+const mission = 10;
+const period = 'Период';
+alert('мой первый урок');
+console.log('Мой первый урок');
