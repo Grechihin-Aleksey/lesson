6981263f37f1e09c0a70863aad56bcd8af2d9c6b
Aleksey Grechihin <9108921170@dz-led.ru>
2020-08-07 13:43:33 +0300

    Домашнее задание второй урок

--- script/script.js
+++ script/script.js
@@ -1,8 +1,26 @@
-let money = 5;
-const income = 'Прибыль';
-let addExpenses = null;
+let money = 50;
+console.log(typeof money);
+const income = "Фриланс";
+console.log(typeof income);
+let addExpenses = "Интернет, Такси, Коммуналка";
+console.log(addExpenses.length);
 const deposit = false;
-const mission = 10;
-const period = 'Период';
-alert('мой первый урок');
-console.log('Мой первый урок');
+console.log(typeof deposit);
+const mission = 100;
+let period = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+let numbers = [];
+for (let i = 1; i <= 12; i++) {
+  numbers.push(i);
+}
+
+//Или при помощи цикла for
+
+let budgetDay = money / 30;
+
+console.log(numbers);
+
+console.log("Период равен " + (period[11] + " месяцев."));
+console.log("Цель заработать " + mission + " рублей.");
+console.log(addExpenses.toLowerCase().split(","));
+console.log(budgetDay);
