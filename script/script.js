54bedc9709c3b553c0fefbcfb88d2c695d8de80a
Aleksey Grechihin <9108921170@dz-led.ru>
2020-08-07 19:59:21 +0300

    Присвоил периоду число

--- script/script.js
+++ script/script.js
@@ -7,7 +7,7 @@ console.log(addExpenses.length);
 const deposit = false;
 console.log(typeof deposit);
 const mission = 100;
-let period = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+let period = 12;
 
 let numbers = [];
 for (let i = 1; i <= 12; i++) {
@@ -20,7 +20,7 @@ let budgetDay = money / 30;
 
 console.log(numbers);
 
-console.log("Период равен " + period[11] + " месяцев.");
+console.log("Период равен " + period + " месяцев.");
 console.log("Цель заработать " + mission + " рублей.");
 console.log(addExpenses.toLowerCase().split(","));
 console.log(budgetDay);
